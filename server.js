386309959d30c9f61b0984bc626e424455e30f98
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2022-02-18 00:38:51 -0300

    config-Heroku

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 //Importa as dependências que acabamos de instalar
+
+//importar o express
 const express = require("express");
-const path = require("path");
-
+//iniciar o express
 const app = express();
-
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/NOME_DO_SEU_PROJETO"));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/NOME_DO_SEU_PROJETO/index.html"));
+//nome da pasta no distque sera feito o biuld
+const appName = 'angular-coffee-api';
+//local onde biuld ira gerar os arquivos
+const outputPath = `${__dirname}/dist/${appName}`;
+//Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(outputPath));
+//Redirecionar qualquer requisição para o index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${outputPath}/index.html`));
 });
 
-// Inicia a aplicação pela porta configurada
+// Inicia a aplicação pela porta configurada e
+//ouvir a porta que o Heroku disponibilizar
 app.listen(process.env.PORT || 8080);
+
+//const path = require("path");
